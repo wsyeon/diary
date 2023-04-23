f8f0e5a559d77a300f055e169857663cc3987fe2
C팀 <sywon2005@gmail.com>
2023-04-23 17:16:42 +0900

    fbase 정리

--- src/fbase.tsx
+++ src/fbase.tsx
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
@@ -13,7 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firebaseAuth = getAuth(app);
 
-export { analytics, firebaseAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
+export { firebaseAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
