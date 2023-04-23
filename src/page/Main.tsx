1b3a4f0a6b42684038547cfe3a3f08a651ac01c1
C팀 <sywon2005@gmail.com>
2023-04-23 17:17:02 +0900

    메인 페이지 제작 시작

--- src/page/Main.tsx
+++ src/page/Main.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firebaseAuth } from '../fbase';
 
 const Main = () => {
+    const [test, setTest] = useState<string>("");
+
+    useEffect(()=> {
+        firebaseAuth.onAuthStateChanged(user=> {
+            if (user) {
+                setTest("logOut");
+            } else {
+                setTest("login");
+            }
+
+        })
+    }, []);
     return (
         <div>
-            
+            {test}
         </div>
     );
 };
