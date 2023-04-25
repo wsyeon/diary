fdea156412b3c234f5c8e1dc086ad5894fe5618a
C팀 <sywon2005@gmail.com>
2023-04-25 23:23:48 +0900

    wire페이지 툴 잡기

---
+++ src/styled/write.tsx
@@ -0,0 +1,52 @@
+import styled from "@emotion/styled";
+
+export const WriteWrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+`;
+
+export const DiaryWrapper = styled.div`
+    margin-top: 5rem;
+    width: 100%;
+    height: 80%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`;
+
+export const DiaryTitleWrapper = styled.div`
+`;
+
+export const DiaryTitleInput = styled.input`
+    border: none;
+    border-bottom: 1px solid #000;
+    outline: none;
+`;
+
+export const DiaryTextWrapper = styled.div`
+`;
+
+export const DiaryText = styled.textarea`
+    resize: none;
+`;
+
+export const DiaryTagsWrapper = styled.div`
+`;
+
+export const DiaryTags = styled.input`
+    outline: none;
+    border: none;
+    border-bottom: 1px solid #000;
+`;
+
+export const DiaryTagsBtn = styled.button`  
+`;
+
+export const WriteBtnWrapper = styled.div`
+`;
+
+export const WriteBtn = styled.button`
+`;
+
